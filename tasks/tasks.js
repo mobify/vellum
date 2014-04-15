689660e3bb49a8a4797989089d4b902a6e0b8fab
Steve Calvert <steve.calvert@gmail.com>
2014-04-15 08:39:38 -0700

    Oops. Rename of default task.

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -1,8 +1,7 @@
-
 module.exports = function(grunt) {
     grunt.registerTask('vellum:build:dev', ['sass:dev', 'autoprefixer']);
     grunt.registerTask('vellum:build:prod', ['sass:prod', 'autoprefixer']);
     grunt.registerTask('vellum:build', ['vellum:build:dev']);
 
-    grunt.registerTask('default', 'adaptive:build');
+    grunt.registerTask('default', 'vellum:build');
 };
